034d5bb224df92169f704328334f45d0e983c2fe
Michael S <tonichaelmight@gmail.com>
2022-07-26 03:41:53 -0400

    remove unnecessary EV refs

--- TwitchCommand.js
+++ TwitchCommand.js
@@ -30,7 +30,7 @@ class TwitchCommand {
 
   async execute(messageObject) {
     //console.log(messageObject);
-    if (!messageObject.tags.mod && !(messageObject.tags.username === ev.CHANNEL_NAME)) {
+    if (!messageObject.tags.mod && !(messageObject.tags.username === messageObject.channel.slice(1))) {
       if (this.modOnly || this.onCooldown) {
         return;
       }
@@ -83,7 +83,7 @@ class TwitchCounterCommand extends TwitchCommand {
     console.log(command);
 
     if (command === this.name) {
-      if (messageObject.tags.mod || messageObject.tags.username === ev.CHANNEL_NAME) {
+      if (messageObject.tags.mod || messageObject.tags.username === messageObject.channel.slice(1)) {
         if (messageWords[1] === 'set') {
           evaluation.action = 'set';
           const newValue = messageWords[2];
@@ -97,7 +97,6 @@ class TwitchCounterCommand extends TwitchCommand {
             evaluation.attempt = newValue;
           }
         } else {
-          console.log('hi');
           evaluation.action = 'add';
           const currentValue = await this.getValue();
           const newValue = currentValue * 1 + 1;
@@ -137,7 +136,7 @@ class TwitchCounterCommand extends TwitchCommand {
 
   async execute(messageObject) {
     //console.log(messageObject);
-    if (!messageObject.tags.mod && !(messageObject.tags.username === ev.CHANNEL_NAME)) {
+    if (!messageObject.tags.mod && !(messageObject.tags.username === messageObject.channel.slice(1))) {
       if (this.modOnly || this.onCooldown) {
         return;
       }
@@ -145,7 +144,7 @@ class TwitchCounterCommand extends TwitchCommand {
 
     let evaluation;
 
-    if (messageObject.tags.mod || messageObject.tags.username === ev.CHANNEL_NAME) {
+    if (messageObject.tags.mod || messageObject.tags.username === messageObject.channel.slice(1)) {
       evaluation = await this.evaluateMessage(messageObject);
     }
 
@@ -373,7 +372,7 @@ async function soCallback(messageObject) {
     recipient = recipient.slice(1);
   }
 
-  if (recipient === ev.CHANNEL_NAME) {
+  if (recipient === messageObject.channel.slice(1)) {
     messageObject.addResponse(
       `@${recipient} is pretty cool, but she doesn't need a shoutout on her own channel.`
     )
@@ -519,13 +518,13 @@ const mute = new TwitchCommand('mute', muteCallback);
 const muted = new TwitchCommand('muted', muteCallback);
 function muteCallback(messageObject) {
   messageObject.addResponse(
-    `@${ev.CHANNEL_NAME.toUpperCase()} HEY QUEEN 👸👸👸 YOU'RE MUTED`
+    `@${messageObject.channel.slice(1).toUpperCase()} HEY QUEEN 👸👸👸 YOU'RE MUTED`
   );
   messageObject.addResponse(
-    `@${ev.CHANNEL_NAME.toUpperCase()} HEY QUEEN 👸👸👸 YOU'RE MUTED`
+    `@${messageObject.channel.slice(1).toUpperCase()} HEY QUEEN 👸👸👸 YOU'RE MUTED`
   );
   messageObject.addResponse(
-    `@${ev.CHANNEL_NAME.toUpperCase()} HEY QUEEN 👸👸👸 YOU'RE MUTED`
+    `@${messageObject.channel.slice(1).toUpperCase()} HEY QUEEN 👸👸👸 YOU'RE MUTED`
   );
 }
 
